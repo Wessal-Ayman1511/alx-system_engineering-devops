a58c0e9bdc29767078cf78848021357c80a129ca
Wessal Ayman <wessalayman237@gmail.com>
2024-07-29 17:30:00 +0300

    json

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Python script that exports data in the CSV format
+"""
+
+from requests import get
+from sys import argv
+import json
+
+
+if __name__ == "__main__":
+    to_do = get('https://jsonplaceholder.typicode.com/todos/')
+    to_do_response = to_do.json()
+    users = get('https://jsonplaceholder.typicode.com/users/')
+    users_response = users.json()
+    
+    final_dic = {}
+    for i in users_response:
+        list = []
+        for j in to_do_response:
+            dic = {}
+            if (i['id'] == j['userId']):
+                dic['task'] = j['title']
+                dic['completed'] = j['completed']
+                dic['username'] = i['username']
+            list.append(dic)
+        final_dic[i['id']] = list
+        
+    print(final_dic)
+
+    #json_serialization = json.dumps(dict)
+
+    # with open(argv[1] + ".json", 'w') as file:
+    #     file.write(json_serialization)
