c53327ded0df467ceb383a6b87e33fc113e9676c
Wessal Ayman <wessalayman237@gmail.com>
2024-07-29 16:07:26 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,18 +9,18 @@ if __name__ == "__main__":
     completed = 0
     total = 0
     tasks = []
-    
+
     for i in users_response:
         if i.get('id') == int(argv[1]):
             employee = i.get('name')
-            
+
     for i in to_do_response:
         if i.get('userId') == int(argv[1]):
             total += 1
-            if i.get('completed') == True:
+            if i.get('completed') is True:
                 completed += 1
                 tasks.append(i.get('title'))
-                
+
     print("Employee {} is done with tasks({}/{}):".format(employee, completed,
                                                           total))
     for i in tasks:
