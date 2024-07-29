f40515339832364478e7e02341f352f71927f64e
Wessal Ayman <wessalayman237@gmail.com>
2024-07-29 16:47:51 +0300

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 """
 Python script that exports data in the CSV format
 """
-
 
 from requests import get
 from sys import argv
@@ -15,18 +13,18 @@ if __name__ == "__main__":
     to_do_response = to_do.json()
     users = get('https://jsonplaceholder.typicode.com/users/')
     users_response = users.json()
-for i in users_response:
-    if i.get('id') == int(argv[1]):
-        employee = i.get('name')
-with open(argv[1] + '.csv', 'w', newline='') as file:
-    writ = csv.writer(file, quoting=csv.QUOTE_ALL)
+    for i in users_response:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
+    with open(argv[1] + '.csv', 'w', newline='') as file:
+        writ = csv.writer(file, quoting=csv.QUOTE_ALL)
 
-    for i in to_do_response:
-        if i['userId'] == int(argv[1]):
-            row = [
-                i['userId'],
-                employee,
-                i['completed'],
-                i['title']
-            ]
-            writ.writerow(row)
+        for i in to_do_response:
+            if i['userId'] == int(argv[1]):
+                row = [
+                    i['userId'],
+                    employee,
+                    i['completed'],
+                    i['title']
+                ]
+                writ.writerow(row)
