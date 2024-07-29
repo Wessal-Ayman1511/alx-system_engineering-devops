a45e2a99fff7e99ef0745725783e22026efe52db
Wessal Ayman <wessalayman237@gmail.com>
2024-07-29 16:04:51 +0300

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    to_do = get('https://jsonplaceholder.typicode.com/todos/')
+    to_do_response = to_do.json()
+    users = get('https://jsonplaceholder.typicode.com/users/')
+    users_response = users.json()
+    completed = 0
+    total = 0
+    tasks = []
+    
+    for i in users_response:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
+            
+    for i in to_do_response:
+        if i.get('userId') == int(argv[1]):
+            total += 1
+            if i.get('completed') == True:
+                completed += 1
+                tasks.append(i.get('title'))
+                
+    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
+                                                          total))
+    for i in tasks:
+        print("\t {}".format(i))
