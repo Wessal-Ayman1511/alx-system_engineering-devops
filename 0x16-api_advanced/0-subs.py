dd5bd4fdd1531218b29885010d61f0241172da85
Wessal Ayman <wessalayman237@gmail.com>
2024-08-13 16:28:48 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
+headers = __import__('try').headers
+
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://oauth.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "PostmanRuntime/7.41.0"
-    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
