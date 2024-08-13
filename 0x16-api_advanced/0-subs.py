9ec4b310a67b77e2d5aa909dad519fdc6bf4e954
Wessal Ayman <wessalayman237@gmail.com>
2024-08-13 16:34:01 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
-headers = {'User_Agent': 'MyAPI/0.0.1', 'Authorization': 'bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzIzNjQyMjUyLjE3MTkwNiwiaWF0IjoxNzIzNTU1ODUyLjE3MTkwNiwianRpIjoiS1YwSjNyenNXdEZyUWEza3IwVzZyOWI2VFhUcnBnIiwiY2lkIjoiVzFUY1h5aEpUaXR6RDd4SWJxZXF3USIsImxpZCI6InQyXzE2a3VnN2tkaGQiLCJhaWQiOiJ0Ml8xNmt1ZzdrZGhkIiwibGNhIjoxNzIzNTUwNzQ5MjA4LCJzY3AiOiJlSnlLVnRKU2lnVUVBQURfX3dOekFTYyIsImZsbyI6OX0.ERKFUjV4GigxOhcJn-KvMzNqEW0bxdfc6FANdfeUowgFoaniNKkGg9kTwgQIdU_EloWOnLd7HDGA1ZytqefKd0GUXPJRrxmlwLmmyGQxuxgY7HSV9QD6CPXAUmw6bAPkJt_O748K5T72D0ZGH6EULv9fwWPLVSj8WFQ40d89u6sQ0vRCbHVQ4PYxiFConSqJ9zx00AttEIuZISHcc-vuXJe2mAmnHdZXzcoYV8iWE0f2DNgfT9K6wEFrKOW03XMWwcNDrksMkqBLsw6Jg5VKEhTBdci39C3zAwz37Pp1NrIZbaOJRdQMV8nAu9X2kXSJ07gCkO6eoWGqop0jtEhmkQ'}
+
+Client_ID = "W1TcXyhJTitzD7xIbqeqwQ"
+SECRET_ID = "KuchN3CxdZhkpeDyDyQdhi2fjY5VGw"
+
+import requests
+from requests import auth
+import requests.auth
+
+auth = requests.auth.HTTPBasicAuth(Client_ID, SECRET_ID)
+
+data = {
+    'grant_type': 'password',
+    'username': 'Wessal-Ayman',
+    'password': 'wessal@1511'
+}
+
+headers = {'User_Agent': 'MyAPI/0.0.1'}
+
+res = requests.post('https://www.reddit.com/api/v1/access_token',
+                    auth=auth, data=data, headers=headers)
+
+TOKEN = res.json()['access_token']
+
+headers['Authorization'] = f'bearer {TOKEN}'
 
 
 
