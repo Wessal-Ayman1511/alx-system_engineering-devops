1b72049f594f36b0272601e9b4433ab4be46cb21
Wessal Ayman <wessalayman237@gmail.com>
2024-08-13 16:47:24 +0300

    top ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Wessal-Ayman)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers)
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
--- 0x16-api_advanced/try.py
+++ 0x16-api_advanced/try.py
@@ -22,6 +22,8 @@ headers = {'User_Agent': 'MyAPI/0.0.1'}
 res = requests.post('https://www.reddit.com/api/v1/access_token',
                     auth=auth, data=data, headers=headers)
 
-TOKEN = res.json()['access_token']
+print(res)
 
-headers['Authorization'] = f'bearer {TOKEN}'
+# TOKEN = res.json()['access_token']
+
+# headers['Authorization'] = f'bearer {TOKEN}'
