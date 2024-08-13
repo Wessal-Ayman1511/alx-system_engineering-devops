793c6723f57a67eaf22d19c0c79a40e1092b0202
Wessal Ayman <wessalayman237@gmail.com>
2024-08-13 15:53:09 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://oauth.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "application/vnd.api+json"
+        "User-Agent": "PostmanRuntime/7.41.0"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
