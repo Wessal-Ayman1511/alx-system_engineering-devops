c8c81301f83f6eda121941da0335a32ea2b642b1
Wessal Ayman <wessalayman237@gmail.com>
2024-08-13 17:19:31 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,10 +14,10 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "linux:myredditbot:v1.0.0 (by /u/wassola)"
     }
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
+
         if response.status_code == 200:
             data = response.json()
             return data["data"]["subscribers"]
