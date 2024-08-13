18434c9697ef652d9de30536c0e6310d8784fce7
Wessal Ayman <wessalayman237@gmail.com>
2024-08-13 17:17:51 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,24 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """
+    Queries the Reddit API and returns
+    the number of subscribers for a given subreddit.
+    :param subreddit: The name of the subreddit (str)
+    :return: The number of subscribers (int), or 0 if the subreddit is invalid
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "PostmanRuntime/7.41.0"
+        "User-Agent": "linux:myredditbot:v1.0.0 (by /u/wassola)"
     }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 404:
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 200:
+            data = response.json()
+            return data["data"]["subscribers"]
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
